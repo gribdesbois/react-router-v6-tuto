4021bfdbc1eccc928a5f499f61acdbed8b8145c4
gribdesbois <gribdesbois@gmail.com>
2021-11-17 12:39:37 +0100

    urlSearchParams default behaviour

--- frontend/src/routes/invoices.jsx
+++ frontend/src/routes/invoices.jsx
@@ -1,8 +1,14 @@
-import { NavLink, Outlet } from 'react-router-dom'
+import {
+  NavLink,
+  Outlet,
+  useSearchParams,
+} from 'react-router-dom'
 import { getInvoices } from '../data'
 
 export default function Invoices() {
-  const invoices = getInvoices();
+  const invoices = getInvoices()
+  const [searchParams, setSearchParams] = useSearchParams()
+
   return (
     <div style={{ display: 'flex' }}>
       <nav
@@ -11,18 +17,39 @@ export default function Invoices() {
           padding: '1rem',
         }}
       >
-        {invoices.map((invoice) => (
-          <NavLink
-
-            className={({ isActive }) => (isActive ? 'red' : 'blue')}
-            to={`/invoices/${invoice.number}`}
-            key={invoice.number}
-          >
-            {invoice.name}
-          </NavLink>
-        ))}
+        <input
+          value={searchParams.get('filter') || ''}
+          onChange={(event) => {
+            const filter = event.target.value
+            if (filter) {
+              setSearchParams({ filter })
+            } else {
+              setSearchParams({})
+            }
+          }}
+        />
+        {invoices
+          .filter((invoice) => {
+            const filter = searchParams.get('filter')
+            if (!filter) return true
+            const name = invoice.name.toLowerCase()
+            return name.startsWith(filter.toLowerCase())
+          })
+          .map((invoice) => (
+            <NavLink
+              style={({ isActive }) => ({
+                display: 'block',
+                margin: '1rem 0',
+                color: isActive ? 'red' : '',
+              })}
+              to={`/invoices/${invoice.number}`}
+              key={invoice.number}
+            >
+              {invoice.name}
+            </NavLink>
+          ))}
       </nav>
       <Outlet />
     </div>
-  );
+  )
 }

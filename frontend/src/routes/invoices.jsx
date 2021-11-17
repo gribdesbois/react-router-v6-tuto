a1ed475c63e5cf5f03d6d33cf0179057f2eafb4c
gribdesbois <gribdesbois@gmail.com>
2021-11-17 11:53:00 +0100

    active link

--- frontend/src/routes/invoices.jsx
+++ frontend/src/routes/invoices.jsx
@@ -1,8 +1,8 @@
-import { Link, Outlet } from 'react-router-dom'
+import { NavLink, Outlet } from 'react-router-dom'
 import { getInvoices } from '../data'
 
 export default function Invoices() {
-  const invoices = getInvoices()
+  const invoices = getInvoices();
   return (
     <div style={{ display: 'flex' }}>
       <nav
@@ -12,16 +12,21 @@ export default function Invoices() {
         }}
       >
         {invoices.map((invoice) => (
-          <Link
-            style={{ display: 'block', margin: '1rem 0' }}
+          <NavLink
+
+            style={({ isActive }) => ({
+              display: 'block',
+              margin: '1rem 0',
+              color: isActive ? 'red' : '',
+            })}
             to={`/invoices/${invoice.number}`}
             key={invoice.number}
           >
             {invoice.name}
-          </Link>
+          </NavLink>
         ))}
       </nav>
       <Outlet />
     </div>
-  )
+  );
 }
